dd554e45b9552146a09cb2a08de73632fcb3d70e
gerryATS <gerry@apptreetechnologies.com>
2016-04-01 20:30:24 +0530

    commit devise logout action

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :orders
   devise_for :admins do
-    delete '/admins/sign_out' => 'devise/sessions#destroy'
+    get '/admins/sign_out' => 'devise/sessions#destroy'
   end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
