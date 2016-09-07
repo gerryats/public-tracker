20c34e8d4dd8930df6114d1d66b491a8f63d9ea7
gerryATS <gerry@apptreetechnologies.com>
2016-09-07 12:48:12 +0530

    Changed devise URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -74,6 +74,8 @@ Rails.application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
+  config.action_mailer.default_url_options = { host: '112.196.19.154', port: 3000 }
+
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
